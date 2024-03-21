3938733e146e3e5fe8dd56cae970536c4696d676
Katya <dolgova01011987@gmail.com>
2024-03-21 22:45:19 +0300

    Initial commit

---
+++ src/main/java/ru/netology/post/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.post;
+
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public FormData birthday;
+    public boolean subscription;
+
+}
+
+
+
+
+
